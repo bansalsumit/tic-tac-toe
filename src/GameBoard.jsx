012fb6f7aeaeee10a0b3b640446b32c820882360
Sumit Bansal <sbansal.mait@gmail.com>
2024-10-09 23:31:16 +0530

    Added player win logic.

--- src/GameBoard.jsx
+++ src/GameBoard.jsx
@@ -1,22 +1,7 @@
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-
-export default function GameBoard({ onSelectSquare, turns }) {
-  let gameBoard = initialGameBoard;
-
-  for (const turn of turns) {
-    const { square, player } = turn;
-    const { row, col } = square;
-
-    gameBoard[row][col] = player;
-  }
-
+export default function GameBoard({ onSelectSquare, board }) {
   return (
     <ol id="game-board">
-      {gameBoard.map((row, rowindex) => (
+      {board.map((row, rowindex) => (
         <li key={rowindex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
