f33b7709ae19a27b5a6a83044065ea30b31dcaed
Sumit Bansal <sbansal.mait@gmail.com>
2024-10-09 23:57:58 +0530

    Added game won will get actual name of player.

--- src/App.jsx
+++ src/App.jsx
@@ -13,18 +13,26 @@ const initialGameBoard = [
 
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = "X";
-
   if (gameTurns.length > 0 && gameTurns[0].player === "X") {
     currentPlayer = "O";
   }
 
   return currentPlayer;
 }
+
 function App() {
+  const [players, setPlayers] = useState({
+    X: "Player 1",
+    O: "Player 2",
+  });
+
   const [gameTurns, setGameTurns] = useState([]);
+
   const activePlayer = deriveActivePlayer(gameTurns);
 
   let gameBoard = [...initialGameBoard.map((array) => [...array])];
+
+  let winner;
 
   for (const turn of gameTurns) {
     const { square, player } = turn;
@@ -32,8 +40,6 @@ function App() {
 
     gameBoard[row][col] = player;
   }
-
-  let winner;
 
   for (const combination of WINNING_COMBINATIONS) {
     const firstSquareSymbol =
@@ -48,7 +54,7 @@ function App() {
       firstSquareSymbol == secondSquareSymbol &&
       firstSquareSymbol == thirdSquareSymbol
     ) {
-      winner = firstSquareSymbol;
+      winner = players[firstSquareSymbol];
     }
   }
 
@@ -70,6 +76,12 @@ function App() {
     setGameTurns([]);
   }
 
+  function handlePlayerNameChange(symbol, newName) {
+    setPlayers((prevPlayers) => {
+      return { ...prevPlayers, [symbol]: newName };
+    });
+  }
+
   return (
     <main>
       <div id="game-container">
@@ -78,11 +90,13 @@ function App() {
             initialName="Player 1"
             symbol="X"
             isActive={activePlayer === "X"}
+            onChangeName={handlePlayerNameChange}
           />
           <Player
             initialName="Player 2"
             symbol="O"
             isActive={activePlayer === "O"}
+            onChangeName={handlePlayerNameChange}
           />
         </ol>
         {(winner || hasDraw) && (
